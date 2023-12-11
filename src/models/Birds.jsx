2726d0d9b93308b6bbce3382c58cebee8c75da2d
ende <1195562121ende@gmail.com>
2023-12-11 11:02:43 +0100

    Portfolio mensaje inicial

---
+++ src/models/Birds.jsx
@@ -0,0 +1,42 @@
+import {useRef, useEffect, React} from 'react'
+import birdScene from '../assets/3d/bird.glb';
+import {useGLTF, useAnimations } from '@react-three/drei';
+import {useFrame} from '@react-three/fiber';
+
+const Birds = () => {
+    const  { scene, animations} = useGLTF(birdScene);
+    const birdRef = useRef();
+    const {actions} = useAnimations(animations, birdRef);
+
+    useEffect(() => {
+      actions['Take 001'].play();
+    },[]);
+
+    useFrame(({clock, camera}) => {
+      // Para simular el movimiento del pájaro en forma de seno(baja y sube)
+      birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+
+      // Para comprobar si el pájaro ha llegado a una posición con respecto a la cámara
+      if(birdRef.current.position.x > camera.position.x + 10){
+        birdRef.current.rotation.y = Math.PI;
+      }else if(birdRef.current.position.x < camera.position.x - 10){
+        birdRef.current.rotation.y = 0;
+      }
+      // Para actualizar la posición x y z basado en la dirección
+      if(birdRef.current.rotation.y === 0){
+        birdRef.current.position.x += 0.01;
+        birdRef.current.position.z -= 0.01;
+      }else{
+        birdRef.current.position.x -= 0.01;
+        birdRef.current.position.z += 0.01;
+      }
+    })
+
+  return (
+    <mesh position={[-5,2,1]} scale= {[0.003, 0.003, 0.003]} ref={birdRef}>
+        <primitive object={scene}/>
+    </mesh>
+  )
+}
+
+export default Birds
